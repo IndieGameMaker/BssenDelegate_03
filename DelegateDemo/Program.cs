2c2d35b42e1c3fc50c688cecd42dd67e0d3da78a
Lee JaeHyun <myleje@gmail.com>
2026-03-10 22:28:55 +0900

    feat: Func 로직 구현

--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -21,17 +21,21 @@ class Program
         // 이벤트를 구독(Subscribe)
         // 무명 델리게이트
         // 람다식  (=> goes to, 람다)
+        // 옵저버 패턴(Observer Pattern)
         player.OnHpChanged += PlayerDamaged;
-        
+
         // 플레이어 사망 메소드 호출
         // 이벤트를 외부 클래스에서 직접 호출할 수 없다.
         // player.OnPlayerDie?.Invoke();
-        
+
         player.TakeDamage(20);
         player.TakeDamage(40);
-        
+
         player.Heal(30);
-        
+
+        // Func: 데미지 계산 공식을 런타임에 교체
+        // 방어구 장착 -> 데미지를 20% 감소
+        player.DamageFormula = (damage) => (int)(damage * 0.8f);
         player.TakeDamage(40);
         
         // 이벤트 해지(Unsubscribe) : 메모리 해제
@@ -68,11 +72,17 @@ class Player
     public event Action<int, int> OnHpChanged;
     
     // Func 델리게이트
-    /*
-     * Func<T1, T2, TResult> 
-     * Func<int, float, int> => Func<현재HP, 회복비율, 회복HP>
+    /* .NET에서 제공하는 내장 델리게이트
+     * 반환값이 있는 델리게이트 : Func<T1, T2, ..., TResult>
+     * 마지막 타입이 반환형 (나머지는 매개변수)
+     *
+     * Func<int, int> => Func<입력데미지, 최종데미지>
+     *
+     * Action 과의 차이:
+     *   Action<int>      => 반환형 없음 (void)
+     *   Func<int, int>   => 반환형 있음 (int)
      */
-    public event Func<int, int, int> OnHealing = (currHp, healAmount) => currHp += healAmount;
+    public Func<int, int> DamageFormula = (damage) => damage; // 기본: 데미지 그대로
     
 
     public Player(string name, int hp)
@@ -85,7 +95,7 @@ class Player
 
     public void Heal(int healAmount)
     {
-        _hp = OnHealing(_hp, healAmount);
+        _hp += healAmount;
         Console.WriteLine($"HP 회복: {_hp} [회복량: {healAmount}]");
     }
     
@@ -97,10 +107,11 @@ class Player
 
     public void TakeDamage(int damage)
     {
-        _hp -= damage;
-        
-        OnHpChanged?.Invoke(damage, _hp); // 이벤트 발생 Raise
-        
+        int finalDamage = DamageFormula(damage); // Func 호출 -> 계산된 최종 데미지
+        _hp -= finalDamage;
+
+        OnHpChanged?.Invoke(finalDamage, _hp); // 이벤트 발생 Raise
+
         if (_hp <= 0)
         {
             OnPlayerDie?.Invoke(); // 이벤트 Raise
